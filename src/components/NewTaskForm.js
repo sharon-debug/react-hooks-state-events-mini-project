c2ee0561942e74fe6cd98f568683891d0c2bd5f7
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-10-06 19:28:37 +0300

    completed

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit,categories}) {
+  const[text,setText]=useState("");
+  const[category,setCategory]=useState("code");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({text,category});
+    setText("");
+    setCategory("code");
+  }
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" onChange={(e)=>setText(e.target.value)}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category"value={category} onChange={(e)=>setCategory(e.target.value) }>
+          {categories.map((cat)=>(
+            <option key={cat}>{cat}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
-
-function TaskList() {
+import Task from "./Task"
+function TaskList({tasks,onDeleteTask}) {
+  const tasklist=tasks.map(task=><Task key={task.text}
+    text={task.text}
+    category={task.category}
+    onDeleteTask={onDeleteTask}
+    />)
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasklist}
     </div>
   );
 }
